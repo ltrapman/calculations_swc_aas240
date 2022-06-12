83823551066e9632e6056ef760882789f96d57ea
Macy Huston <mzh616@psu.edu>
2022-06-12 16:30:04 -0700

    made date calculator less bad

--- date_calculator.py
+++ date_calculator.py
@@ -1,37 +1,14 @@
-LEAST_FAVORITE_YEAR = 2020
-
-def suPerScr_etFunCtIon(month, day, year):
+def day_of_year(month, day, year):
     """
     returns the day of year given a date
     """
 
-    magic = day
-    if month == 2:
-        magic = magic + 31
-    elif month == 3:
-        magic = magic + 59
-    elif month == 4:
-        magic = magic + 90
-    elif month == 5:
-        magic = magic + 31 + 28 + 31 + 30
-    elif month == 6:
-        magic = magic + 31 + 28 + 31 + 30 + 31
-    elif month == 7:
-        magic = magic + 31 + 28 + 31 + 30 + 31 + 30
-    elif month == 8:
-        magic = magic + 31 + 28 + 31 + 30 + 31 + 30 + 31
-    elif month == 9:
-        magic = magic + 31 + 28 + 31 + 30 + 31 + 30 + 31 + 31
-    elif month == 10:
-        magic = magic + 31 + 28 + 31 + 30 + 31 + 30 + 31 + 31 + 30
-    elif month == 11:
-        magic = magic + 31 + 28 + 31 + 30 + 31 + 30 + 31 + 31 + 30 + 31
-    elif month == 12:
-        magic = magic + 31 + 28 + 31 + 30 + 31 + 30 + 31 + 31 + 30 + 31 + 31
-    if year == LEAST_FAVORITE_YEAR:
-        magic = -1
-    print(magic)
-    return magic
+    days_in_month = [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]
+    day_num = day + sum(days_in_month[0:month-1])
+    if year % 4 == 0: #leap year
+        day_num +=1
+
+    return day_num
 
 if __name__ == "__main__":
-    print(suPerScr_etFunCtIon(4, 1, 2022))
+    print(day_of_year(4, 1, 2022))
